ccfd137c1c0c73002363c70c3f6fe8c12d6cbcab
Pivanchikov <56319613+Pivanchikov@users.noreply.github.com>
2020-09-10 16:07:33 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  function check(str, bracketsConfig) {
+  let array = []
+  str.split('').forEach((item, index) => {
+
+    if (item === '(' || item === '[' || item === '{') {
+      array.push(item)
+    }
+    if (item === ')'  && array[array.length - 1] === '(') {
+      array.pop()
+    }
+    if (item === ']'  && array[array.length - 1] === '[') {
+      array.pop()
+    }
+    if (item === '}'  && array[array.length - 1] === '{') {
+      array.pop()
+    }
+  })
+ return  array.length ? false : true
 }
